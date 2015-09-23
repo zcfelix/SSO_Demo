c022dede63f2b238a93e3d6bc58c1f9977a8e0a6
zcfelix <felix-zhou@FelixZhoudeMacBook-Air.local>
2015-09-24 07:34:30 +0800

    add database.yml to repo

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -16,37 +16,43 @@ class StaticPagesController < ApplicationController
   end
 
   def handle_cookies 
-    if !cookies[:sso_token].nil?
-      redirect_to "https://www.baidu.com"
-      #redirect_to "https://www.haohao.com/index.php?r=me.com/token?token=xxxx"
+    #login_server = "http://192.168.1.21/index.php?r=192.168.1.13:8080/token"
+    if cookies[:sso_token].nil?
+      redirect_to "http://192.168.1.21/index.php?r=192.168.1.13:8080/token"
       # 展示重定向页面
 
     # has token, sent to vertify   
     else
       t = cookies[:sso_token]
-      url = "https://baidu.com?token=#{t}"
+      #url = "https://baidu.com?token=#{t}"
+      url = "https://vertify_server/token/#{t}"
       json = send_to_vertify(url)
       if json[:status] == true
-        # 展示页面
+        render
       else
-        redirect_to "https://www.baidu.com"
+        redirect_to "http://192.168.1.21/index.php?r=192.168.1.13:8080/token"
       end
     end
   end
 
   def send_to_vertify(url)
+    user_cert_file = Rails.root.join('app.pem')
+    user_key_file = user_cert_file
+
     http = HTTPClient.new
+    http.ssl_config.set_client_cert_file(user_cert_file, user_key_file)
     response = http.get(url)
-    #response.body 
-    {name: "Felix", id: 123}
+    response.body 
+    #{name: "Felix", id: 123}
   end
 
   def token
     t = params[:token]
-    url = "https://www.baidu.com?token=#{t}"
+    url = "https://verify_server/token/#{t}"
+    #url = "https://www.baidu.com?token=#{t}"
     json = send_to_vertify(url)
-    #@current_user = json[:user_data]
-    @current_user = { name: "Felix", id: 2 }
+    @current_user = json[:user_data]
+    #@current_user = { name: "Felix", id: 2 }
     session[:current_user] = @current_user
     cookies[:sso_token] = t
     render
