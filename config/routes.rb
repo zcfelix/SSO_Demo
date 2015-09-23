85e156aa596642257475ab25f66d3ac740cc6a84
zcfelix <felix-zhou@FelixZhoudeMacBook-Air.local>
2015-09-23 21:06:25 +0800

    Finish static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Demo::Application.routes.draw do
+  root to: 'static_pages#home'
+  match '/redirect', to: 'static_pages#redirect', via: 'get'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
